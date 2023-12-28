fe48692188e07196f2aa227c52d262f03622a18c
ImnotHalsey <147918628+ImnotHalsey@users.noreply.github.com>
2023-12-28 12:28:30 +0530

    main function added

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 from cam import take_photo
 import uos, machine, gc, time
+from utilities import get_timestamp, flasher
 from wifimanager import Call_Manager
 from send_server import upload_photo, hit_api_with_json
 
 wifimanager = Call_Manager()
-if wifimanager:
-    print("Connected to WiFi...")
-    uos.mount(machine.SDCard(), "/SD")
-    while 1:
-        status, path = take_photo(flash=1)
-        if status: 
-            upload_photo(path)
-            j_son = {"Bot_ID": 7, "Temperature" : 2, "Humidity" : 23, "Time": 34}
+
+def runner():
+    if wifimanager:
+        uos.mount(machine.SDCard(), "/SD")
+
+        while 1:
+            status, path = take_photo(flash=1)
+            if status:upload_photo(path);gc.collect()
+            else:print("Bad Error");gc.collect()
+            
+            j_son = {"Bot_ID": 2, "Temperature" : 2, "Humidity" : 23, "Time": get_timestamp()}
             hit_api_with_json(j_son)
-            gc.collect()
             time.sleep(3)
-        else:
-            print("Bad Error")
-            gc.collect()
-else:
-    print("Error while connecting to WiFi")
+    else:
+        print("Error while connecting to WiFi")
 
-
+        
+runner()
